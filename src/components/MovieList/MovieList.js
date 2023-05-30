799323ba094df893b0f57ae02619239b3d573210
Stanislav <Stas.zaliiski@gmail.com>
2023-05-30 18:09:00 +0200

    main

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -52,6 +52,7 @@ const MovieList = () => {
     if (value !== '') {
       handleSubmit({ preventDefault: () => {} });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
